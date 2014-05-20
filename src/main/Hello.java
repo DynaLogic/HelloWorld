ab7d93c23f5246847f43b9e3fc9ca1cdf26887cf
Johnsmith0508 <loganwaldman@gmail.com>
2014-05-20 13:08:54 -0400

    add all files

---
+++ src/main/Hello.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("#YOLOSwagFTW");
+		System.out.println(" That is how it work, right");
+		System.out.println("oh, yah Hello World");
+
+	}
+
+}
